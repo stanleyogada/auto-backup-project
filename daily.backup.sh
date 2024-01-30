11717313d12029fd014310504b22b97a0fc31002
ZERO PACKET LOSS <stanleyogada2000@gmail.com>
2024-01-30 19:05:03 +0100

    Complete implementation of the daily backup script
    - Add remote host arguement

--- daily.backup.sh
+++ daily.backup.sh
@@ -4,24 +4,26 @@
 # Date Created: 29/01/2024
 # Last Modified: 30/01/2024
 #
+#
 # Description
 # - Performs a daily backup of the provided important file/s.
 # - Compresses with today's date, the important file/s with gzip + tar programs.
 # - All the compressions are stored in a backup dir in your HOME directory. (~/backups/daily/)
+# - Note: You must have SSH key setup on the remote host. Find the backup dir also in the Home directory of the remote user. (~/backups)
 #
 # Usage
 # (1) If you wish to backup one file for example /home/user/web-app is the file provide it as a string (between quotes) as the positional arguement number 1.
 # ---
-# ./daily.backup.sh "/home/user/web-app"
+# ./daily.backup.sh "/home/user/web-app" "user@198.168.1.0"
 # ---
-#
 #
 # (2) If you wish to backup multiple files same as the above step but add whitespaces as seperator still withing the string.
 # ---
-# ./daily.backup.sh "/home/user/web-app /home/user/Videos /tmp/my-temp-files"
+# ./daily.backup.sh "/home/user/web-app /home/user/Videos /tmp/my-temp-files" "user@198.168.1.0"
 # ---
 
 all_important_files_paths="$1";
+remote_destination="$2"
 
 # Loops through the important files paths and do for each important file, create its own directory in the backup directory and save the compression there
 for important_file_path in $(echo "$all_important_files_paths"); do
@@ -42,6 +44,9 @@ for important_file_path in $(echo "$all_important_files_paths"); do
 	fi
 done;
 
-#find "$backup_path" -mtime +7 -type f -delete;
+declare -r BACKUP_PATH="$HOME/backups";
 
-#rsync --delete -a --mkpath "$backup_path" "vps@54.163.143.62:~/backups/$important_file_name/";
+find "$BACKUP_PATH" -mtime +7 -type f -delete;
+
+# Synchronize local backup with the remote destination backup
+rsync --delete -a --mkpath "$BACKUP_PATH/" "$remote_destination:~/backups/";
--- init.backup.sh
+++ init.backup.sh
@@ -7,34 +7,44 @@
 # Description
 # - This script adds all the backup scripts (daily, weekly, monthly) to the crontab. Hence eliminating the need for adding the backups scripts manually to the crontab.
 # - All the compressions are stored in a backup dir in your HOME directory. (~/backups/)
+# - Note: You must have SSH key setup on the remote host. Find the backup dir also in the Home directory of the remote user. (~/backups)
 #
 # Usage
 # # (1) If you wish to backup one file for example /home/user/web-app is the file provide it as a string (between quotes) as the positional arguement number 1.
 # ---
-# ./init.backup.sh "/home/user/web-app"
+# ./init.backup.sh "/home/user/web-app" "user@198.168.1.0"
 # ---
 #
 #
 # (2) If you wish to backup multiple files same as the above step but add whitespaces as seperator still withing the string.
 # ---
-# ./init.backup.sh "/home/user/web-app /home/user/Videos /tmp/my-temp-files"
+# ./init.backup.sh "/home/user/web-app /home/user/Videos /tmp/my-temp-files" "user@198.168.1.0"
+
 # ---
 #
 #
-# The positional arguement number 1 should be a string, and can multiple path of directories of files to be backed up seperated by spaces. 
+# The positional arguement number 1 should be a string, and can have multiple path of directories of files to be backed up seperated by spaces.
+#
+# The positional arguement number 2 should be a string, and should the a remote host.
 
 
-file_or_dir_to_backup="$1";
+all_important_files_paths="$1";
+remote_host="$2"
 
 # Terminate if $1 is not provided
-if [[ -z $file_or_dir_to_backup ]]; then
-	echo "Provide the path to the backup directoy";
-	echo "Usage: $0 /path-to-backup";
+if [[ -z $all_important_files_paths || -z $remote_host ]]; then
+	echo "Provide the path to of all important file in a string and a remote host";
+	echo "Usage: $0 \"/path-to-backup /another-important-file\" \"user@192.168.1.0\"";
 	exit 1;
 fi;
 
 # Add the daily script to `/usr/local/bin` so the scrip can be execute with the full path
 if [[ ! -e /usr/local/bin/daily.backup.sh ]]; then
+	if [[ ! -e ./daily.backup.sh ]]; then
+		echo "cd to the project's directory first. You can only run $0 from inside it parent directory";
+		exit 1;
+	fi;
+
 	sudo cp ./daily.backup.sh /usr/local/bin/;
 	sudo chmod 777 $(which daily.backup.sh); 
 fi;
@@ -42,11 +52,11 @@ fi;
 # If no cronjob is found, write the cron job to the crontab file with daily (02:00 AM)
 has_daily_cron="$(crontab -l | grep daily)";
 if [[ -z $has_daily_cron ]]; then
-	echo -e "$(crontab -l)\n0 2 * * * daily.backup.sh $file_or_dir_to_backup" | crontab -;
-	echo "A daily backup of $file_or_dir_to_backup will commence by 02:00 AM"
+	echo -e "$(crontab -l)\n0 2 * * * daily.backup.sh \"$all_important_files_paths\" \"$remote_host\"" | crontab -;
+	echo "A daily backup of $all_important_files_paths will commence by 02:00 AM"
 else
 	echo "Already has a daily cron running already!"
-	echo "You might need to add the cron manually!"
+	echo "You might need to edit the cron manually!"
 fi;
 
 exit 0;
