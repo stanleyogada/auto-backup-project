22bc4d66165365cbcfb60f95a924a20171081c62
Stanley Chinedu Ogada <102979724+stanleyogada@users.noreply.github.com>
2024-02-01 03:20:24 +0100

    Update init.backup.sh

--- init.backup.sh
+++ init.backup.sh
@@ -75,7 +75,7 @@ for element in "${all_type[@]}"; do
 
 		echo "A $script_type backup of $all_important_files_paths will commence by $cron_summary"
 	else
-		echo "Already has a $script_type cron running already!"
+		echo "Already has a $script_type cron running!"
 		echo "You might need to edit the crontab manually using \`crontab -e\`"
 	fi;
 done;	
